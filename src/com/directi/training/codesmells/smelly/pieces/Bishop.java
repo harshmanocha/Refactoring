25920d44826671a08c22d8f27995136bebd9d409
harshmanocha <harsh.man@flock.co>
2017-07-09 06:59:37 +0530

    Fix imports

--- src/com/directi/training/codesmells/smelly/pieces/Bishop.java
+++ src/com/directi/training/codesmells/smelly/pieces/Bishop.java
@@ -1,7 +1,6 @@
 package com.directi.training.codesmells.smelly.pieces;
 
 import com.directi.training.codesmells.smelly.chess.Color;
-import com.directi.training.codesmells.smelly.chess.Position;
 
 public abstract class Bishop extends Piece {
 
--- src/com/directi/training/codesmells/smelly/pieces/Knight.java
+++ src/com/directi/training/codesmells/smelly/pieces/Knight.java
@@ -1,7 +1,6 @@
 package com.directi.training.codesmells.smelly.pieces;
 
 import com.directi.training.codesmells.smelly.chess.Color;
-import com.directi.training.codesmells.smelly.chess.Position;
 
 public abstract class Knight extends Piece {
     public Knight(Color color) {
